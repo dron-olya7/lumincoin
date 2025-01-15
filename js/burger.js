8f671acdc945b8da74975a64b300bafd3e9e16ed
Olga Dron <olya_sok00@mail.ru>
2025-01-15 21:00:05 +0500

    finalizing the project, knitting the frontend with the backend

--- js/burger.js
+++
@@ -1,8 +0,0 @@
-let burger = $("#burger");
-let sidebar = $("#col-auto");
-
-$(document).ready(function(){
-    $(burger).on("click", function(){
-        $(sidebar).toggleClass("open");
-    })
-})
